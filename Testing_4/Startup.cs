5377f7988e47bd8072363d46e032c339676a2594
javvadisairamsagar <44025118+javvadisairamsagar@users.noreply.github.com>
2018-10-11 18:43:42 +0530

    Testing_4
    Testing_4

---
+++ Testing_4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Testing_4.Startup))]
+namespace Testing_4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
